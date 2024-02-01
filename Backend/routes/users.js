e3ada2f3afe71b6c48317c9e7abe6a3ca1da5c11
Chamod Perera <chamod914@gmail.com>
2024-02-01 12:05:09 +0530

    Update users.js

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -3,7 +3,7 @@ let user = require('../models/user.model');
 
 router.route('/').get((req, res) => {
     user.find()
-        .then(user => res.json(users))
+        .then(user => res.json(user))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
