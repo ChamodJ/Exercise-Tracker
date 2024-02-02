2b293f6c4f8cc888cac555b69740a2f2ba0be2b9
Chamod Perera <chamod914@gmail.com>
2024-02-02 14:52:01 +0530

    Update

--- Frontend/Exercise-Tracker-App/src/App.jsx
+++ Frontend/Exercise-Tracker-App/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {BrowserRouter as Router, Route} from "react-router-dom"
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Navbar />
+      <br />
+      <Route path='/' exact Component={ExercisesList} />
+      <Route path='/edit/:id' exact Component={EditExercises} />
+      <Route path='/create' exact Component={CreateExercises} />
+      <Route path='/user' exact Component={CreateUser} />
+    </Router>    
+  );
 }
 
 export default App
+ 
