e076ce31bc51bbd2f8dab4ca615769d5b398a2d8
Chamod Perera <chamod914@gmail.com>
2024-02-03 13:06:38 +0530

    Update

--- Frontend/Exercise-Tracker-App/src/Components/navbar.jsx
+++ Frontend/Exercise-Tracker-App/src/Components/navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='flex align-center justify-start'>
+    <nav className='flex align-center justify-start bg-slate-800 text-white py-[15px] px-[10px]  top-0'>
         <Link to="/" className=''>ExerTracker</Link>
         
-        <div className='flex justify-between list-none '>
+        <div className='flex-1 flex justify-around list-none '>
             <li className=''>
                 <Link to="/" className=''>Exersices</Link>
             </li>
