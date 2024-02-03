76f858320630b3685628b8abfee07b73d1b24d6c
Chamod Perera <chamod914@gmail.com>
2024-02-04 00:29:08 +0530

    Update CreateExercises.jsx

--- Frontend/Exercise-Tracker-App/src/Components/CreateExercises.jsx
+++ Frontend/Exercise-Tracker-App/src/Components/CreateExercises.jsx
@@ -1,18 +1,42 @@
-constructor(props) {
-  super(props);
+import { useState } from "react";
 
-  this.state = {
+const CreateExercises = () => {
+  const [state, setState] = useState({
     username: '',
     description: '',
     duration: 0,
     date: new Date(),
     user: []
-  }
-}
+  })
 
-const CreateExercises = () => {
+  const onChangeUsername = (e) => {
+    setState({
+      ...state,
+      username: e.target.value,
+    });
+  };
+
+
   return (
-    <div>CreateExercises</div>
+    <div>
+      <h3>Create New Exercise Log</h3>
+      <from >
+        <div>
+          <label>Username: </label>
+          <select ref="userInput" required className=""
+            value={this.state.username}
+            onChange={onChangeUsername}>
+              {
+                this.state.user.map(function(user) {
+                  return
+                  <option key={user} value={user}>{user}</option>
+                })
+              }
+
+          </select>
+        </div>
+      </from>
+    </div>
   )
 }
 
