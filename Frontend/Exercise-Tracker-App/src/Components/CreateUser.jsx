b10149a673a4594bff906d2559fbdb7be812692c
Chamod Perera <chamod914@gmail.com>
2024-02-05 16:03:23 +0530

    Update CreateUser.jsx

--- Frontend/Exercise-Tracker-App/src/Components/CreateUser.jsx
+++ Frontend/Exercise-Tracker-App/src/Components/CreateUser.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react"
 
 const CreateUser = () => {
+
+  const [state, setState] = useState({
+    username: ""
+  })
+
+  const onChangeUsername = (e) => {
+    setState({
+      ...state,
+      username: e.target.value
+    })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const user = {
+      username: state.username
+    }
+
+    console.log(user)
+
+    setState({
+      ...state,
+      username : ''
+    })
+  }
+
   return (
-    <div>CreateUser</div>
+    <div>
+    <form onSubmit={onSubmit}>
+        <div className="flex flex-col mb-[10px]">
+          <label className="mb-[5px] font-semibold">Description: </label>
+          <input
+            type="text"
+            value={state.username}
+            className="border-2"
+            onChange={onChangeUsername}
+          />
+        </div>
+        <button type="submit" className="bg-blue-600 text-white px-[10px] py-[5px] rounded">Create Exercise Log</button>
+      </form>
+    </div>
   )
 }
 
